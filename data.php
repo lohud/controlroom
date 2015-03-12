c914ef912a6dbb962eb533d46af9e238ce2278fa
tjk911 <kai@ink-cafe.com>
2015-03-12 14:48:23 -0400

    Work on workaround for API throttling

--- data.php
+++ data.php
@@ -7,7 +7,9 @@ UPDATED: 3/3/15
 
     $apikey = "4201d1d252a2427fbb77572afe2a5782";
     $ny511 = file_get_contents("https://api.511ny.org/api/GetCameras?key={$apikey}&format=json");
-    
-    header('Content-Type: application/json');
+    $ny511encoded = json_encode($ny511);
+    $encodeJSON = file_put_contents('roadways.json', $ny511encoded);
     echo $ny511;
+    header('Content-Type: application/json');
+
 ?>
