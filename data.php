579d5cf2bce1fb75bc94ec862f8f9f7eb040b7d3
tjk911 <kai@ink-cafe.com>
2015-03-12 15:48:37 -0400

    Deploy temporary workaround

--- data.php
+++ data.php
@@ -7,9 +7,29 @@ UPDATED: 3/3/15
 
     $apikey = "4201d1d252a2427fbb77572afe2a5782";
     $ny511 = file_get_contents("https://api.511ny.org/api/GetCameras?key={$apikey}&format=json");
-    $ny511encoded = json_encode($ny511);
-    $encodeJSON = file_put_contents('roadways.json', $ny511encoded);
-    echo $ny511;
+
+    // if (array_key_exists('"Msg"', $ny511)) {
+    // 	echo "Shit";
+    // } else {
+    // 	$encodeJSON = file_put_contents('roadways.json', $ny511);
+    // 	echo "Hooray!";
+    // }
+
+    if (strpos($ny511,'Msg') !== false) {
+        $encodeJSON = file_put_contents('roadways.json', $ny511);
+    }
+
+
+    // for ($i=0; $i<count($ny511);$i++){
+    // 	if (array_key_exists('"Msg"', $ny511)) {
+    // 		echo "Shit";
+    // 	} else {
+    // 		$encodeJSON = file_put_contents('roadways.json', $ny511);
+    // 		echo "Hooray!";
+    // 	}
+    // }
+    
+    // echo $ny511;
     header('Content-Type: application/json');
 
 ?>
