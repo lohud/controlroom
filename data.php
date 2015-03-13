336292680e6f3d6e8217e2c0884714b8f5afad21
tjk911 <kai@ink-cafe.com>
2015-03-13 12:36:21 -0400

    Check json age before pinging api

--- data.php
+++ data.php
@@ -5,15 +5,32 @@ BY: JKTEOH
 UPDATED: 3/3/15
 *****************/
 
-    $apikey = "4201d1d252a2427fbb77572afe2a5782";
-    // $ny511 = file_get_contents("https://api.511ny.org/api/GetCameras?key={$apikey}&format=json");
-
-    if (strpos($ny511, 'Msg') !== FALSE)
-        echo 'Found it';
-    else
-        $encodeJSON = file_put_contents('roadways.json', $ny511);
-
-
+    $filename = 'roadways.json';
+    date_default_timezone_set('America/New_York');
     header('Content-Type: application/json');
+
+    //Get time file was written
+    $filetime = filemtime($filename);
+
+    //Compare values, if filetime is over a minute then ping api to get new data
+    if (time() - $filetime >= 60){
+        //Older than a minute, requery the api
+        /*DEBUG*/
+        // echo "FRESH";
+        /*DEBUG*/
+            
+        $apikey = "4201d1d252a2427fbb77572afe2a5782";
+        $ny511 = file_get_contents("https://api.511ny.org/api/GetCameras?key={$apikey}&format=json");
+        file_put_contents('roadways.json', $ny511);
+        echo $ny511;
+    }
+    else{
+        //Not older, safe to serve up cached data
+        /*DEBUG*/
+        // echo "CACHED";
+        /*DEBUG*/
+        $ny511 = file_get_contents("roadways.json");
+        echo $ny511;
+    }
 
 ?>
